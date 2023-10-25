36ea914f1840ce510bc21eaff04ac0b1d3bcfff4
Коротких Павел <vylout2016@yandex.ru>
2023-10-25 16:02:18 +0500

    fix main-svc

--- main-service/src/main/java/ru/practicum/utils/Сonstants.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.utils;
-
-import java.time.format.DateTimeFormatter;
-
-public class Сonstants {
-    public static final DateTimeFormatter DATE_TIME_PATTERN = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-}
