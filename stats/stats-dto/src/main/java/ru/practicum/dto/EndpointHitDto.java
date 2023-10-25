c4d05a4f7bbe613ae17595a3531560a436848178
Коротких Павел <vylout2016@yandex.ru>
2023-10-25 16:56:40 +0500

    fix main-svc

--- stats/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -1,11 +1,9 @@
 package ru.practicum.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
 
 @Data
 @NoArgsConstructor
